ba0661c40c7094d39d6cec5f4d4e618f0f367912
naran gyawali <nryng123@gmail.com>
2023-10-10 23:33:37 +0545

    fix: some css adjustments

--- src/page/Details.jsx
+++ src/page/Details.jsx
@@ -14,25 +14,28 @@ const data = useContext(ContextData)
   return (
     <>
       <h1>Details</h1>
-      <div className="container">
         <div>
           <ul className={style.ul}>
             <Link to="/"> Go Shopping</Link>
             <Link to="/cart">Check Cart</Link>
           </ul>
         </div>
+      <div className={style.container}>
+        <div>
+
         <h1 className={style.center}>{storeProducts[id].title}</h1>
         <img className= {style} src={`/assets/img/product-${id }.png`} alt="image" />
-        <div>
-          <h2>{storeProducts[id].price} $</h2>
+          <h2 style={{textAlign:"center",marginTop:"25px"}}>{storeProducts[id].price} $</h2>
         </div>
+        <div>
         
         <div className={style.actions}>
-          <input type="button" value=" - 1" onClick={()=>{data.dispatchItems({id:data.present,type:"dec"})}} />
-          <h3>{data.getIndividual(data.present)?.[0]}</h3>
+          <input style={{marginRight:"20px"}} type="button" value=" - 1" onClick={()=>{data.dispatchItems({id:data.present,type:"dec"})}} />
           <input type="button" value=" + 1" onClick={()=>{data.dispatchItems({id:data.present,type:"inc"})}} />
         </div>
-        <h3 className={style.float}>{data.getIndividual(data.present)[1]} $</h3>
+          <h3 style={{textAlign:"right",display:"block",height:"35px"}}>{data.getIndividual(data.present)?.[0]}</h3>
+        <h3 style={{textAlign:"right"}}>{data.getIndividual(data.present)[1]} $</h3>
+        </div>
       </div>
     </>
   );
